cc69e6ef64f704b7c43118851373a4fb49d52e50
Olga <muanaire@gmail.com>
2022-12-25 22:14:56 +0000

    задачи семинара 2

---
+++ задача10/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+Console.Write("Введите число" );
+int n = int.Parse(Console.ReadLine());
+
+if(n > 99)
+{
+    Console.WriteLine((n/10)%10);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
---
+++ задача13/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число" );
+string str = Console.ReadLine();
+Console.WriteLine( str[2]);
+
